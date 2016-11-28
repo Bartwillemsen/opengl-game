4ff92b4b3a499f6e96d556c2b2d94f269f360331
Bart Willemsen <b.willemsen8@gmail.com>
2016-11-29 00:37:22 +0100

    Just testing!

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -10,6 +10,10 @@ import renderEngine.*;
 import models.RawModel;
 import textures.ModelTexture;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MainGameLoop
 {
 	public static void main(String[] args)
@@ -19,18 +23,23 @@ public class MainGameLoop
 		Loader loader = new Loader();
 
 		// Load the OBJ model.
-		RawModel model = OBJLoader.loadObjModel("dragon", loader);
+		RawModel model = OBJLoader.loadObjModel("cube", loader);
 
-		TexturedModel texturedModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("white")));
-		ModelTexture texture = texturedModel.getTexture();
+		TexturedModel cubeModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("brick-texture")));
 
-		texture.setShineDamper(25);
-		texture.setReflectivity(1);
-
-		Entity entity = new Entity(texturedModel, new Vector3f(0, -3, -25), 0, 0, 0, 1);
 		Light light = new Light(new Vector3f(200, 200, 100), new Vector3f(1, 1, 1));
-
 		Camera camera = new Camera();
+
+		List<Entity> cubes = new ArrayList<>();
+		Random random = new Random();
+
+		for (int i = 0; i < 200; i++) {
+			float x = random.nextFloat() * 100 - 50;
+			float y = random.nextFloat() * 100 - 50;
+			float z = random.nextFloat() * -300;
+
+			cubes.add(new Entity(cubeModel, new Vector3f(x, y, z), random.nextFloat() * 180f, random.nextFloat() * 180f, 0, 2f));
+		}
 
 		MasterRenderer renderer = new MasterRenderer();
 		while (! Display.isCloseRequested()) {
@@ -38,7 +47,9 @@ public class MainGameLoop
 
 			// Start the shader program. Render the model, and finally stop
 			// it again.
-			renderer.processEntity(entity);
+			for (Entity c : cubes) {
+				renderer.processEntity(c);
+			}
 
 			renderer.render(light, camera);
 			DisplayManager.updateDisplay();
